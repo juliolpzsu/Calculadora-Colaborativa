4e6ac4f5148105554ee9b226bb4d95d7ee33f9b2
unknown <julio.lopez.suarez.03@gmail.com>
2026-04-28 20:57:26 +0200

    HotFix: Previene división entre cero

--- Division.java
+++ Division.java
@@ -1,6 +1,10 @@
 Java
 public class Division {
     public static double calcular(double a, double b) {
+        if (b == 0) {
+            System.out.println("ERROR: No se puede dividir entre 0.");
+            return 0;
+        }
         return a / b;
     }
 }
---
+++ Multiplicacion.java
@@ -0,0 +1,6 @@
+Java
+public class Multiplicacion {
+    public static double calcular(double a, double b) {
+        return a * b;
+    }
+}
