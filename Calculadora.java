15ae804feffad5b0edfe9cba08583c5278d2f969
unknown <julio.lopez.suarez.03@gmail.com>
2026-04-28 20:36:58 +0200

    Añade Multiplicacion

--- Calculadora.java
+++ Calculadora.java
@@ -4,7 +4,7 @@ public class Calculadora {
         System.out.println("=== CALCULADORA COLABORATIVA ===");
         System.out.println("1. Suma");
         System.out.println("2. Resta");
-        // ESPACIO PARA EL MENÚ
+        System.out.println("3. Multiplicación (Hecho por A)");
         System.out.println("Fin de la ejecución.");
     }
 }
