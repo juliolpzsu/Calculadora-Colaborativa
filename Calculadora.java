0de3a57f6eaadda508d2bccb9468093c44fb5c38
unknown <julio.lopez.suarez.03@gmail.com>
2026-04-28 20:26:21 +0200

    Commit inicial: Clase Principal Calculadora

---
+++ Calculadora.java
@@ -0,0 +1,9 @@
+Java
+public class Calculadora {
+    public static void main(String[] args) {
+        System.out.println("=== CALCULADORA COLABORATIVA ===");
+        // ESPACIO PARA EL MENÚ
+        System.out.println("Fin de la ejecución.");
+    }
+}
+
