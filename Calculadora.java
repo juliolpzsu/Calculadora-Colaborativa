782f37a395a570755e3885f0ed31bbfbab519def
unknown <julio.lopez.suarez.03@gmail.com>
2026-04-28 20:33:26 +0200

    Añade clase y funcionalidad Resta

--- Calculadora.java
+++ Calculadora.java
@@ -3,6 +3,7 @@ public class Calculadora {
     public static void main(String[] args) {
         System.out.println("=== CALCULADORA COLABORATIVA ===");
         System.out.println("1. Suma");
+        System.out.println("2. Resta");
         // ESPACIO PARA EL MENÚ
         System.out.println("Fin de la ejecución.");
     }
---
+++ Resta.java
@@ -0,0 +1,6 @@
+Java
+public class Resta {
+    public static double calcular(double a, double b) {
+        return a - b;
+    }
+}
